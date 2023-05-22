ec2ac3af8b40eebdccfe00097125473feae56a94
DanieleCorrado <corrado_daniele@virgilio.it>
2023-05-22 14:43:47 +0200

    add tasks list with strikethrough text

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-console.log("prova");
+const { createApp } = Vue;
+
+createApp({
+
+  data() {
+    return {
+      tasks: [
+        {
+          text: "prova",
+          done: false
+        },
+        {
+          text: "prova2",
+          done: false
+        },
+        {
+          text: "prova3",
+          done: true
+        },
+        {
+          text: "prova4",
+          done: false
+        },
+      ]
+    }
+  }
+}).mount("#app")
+
